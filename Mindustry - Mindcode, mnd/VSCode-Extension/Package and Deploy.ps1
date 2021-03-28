8e8d012a0863610be319c66dbe4cd8a2fbfabec2
Jehoschua <SmartGit@jig.ch>
2021-03-28 13:34:18 +0200

    Added Package and Deploy.ps1

--- Mindustry - Mindcode, mnd/VSCode-Extension/Package and Deploy.ps1
+++ Mindustry - Mindcode, mnd/VSCode-Extension/Package and Deploy.ps1
@@ -1,5 +1,20 @@
 # Deploy der VSCode Extension zum Marketplace
+# 001, 210328 130720
+
+# Config
+$Publisher = 'TomSchi'
 $ExtensionDir = 'mindcode'
+
+
+Function Get-vsce-Logins() {
+	vsce ls-publishers
+}
+
+
+
+
+## Main
+
 
 $Res = pwd
 Write-Host 'Arbeits-Verzeichnis:' -ForegroundColor Magenta
@@ -14,10 +29,15 @@ Write-Host "`n1/3: Paketieren" -ForegroundColor Yellow
 vsce package
 
 Write-Host "`n2/3: Login" -ForegroundColor Yellow
-vsce login TomSchi
+If ((Get-vsce-Logins).Contains($Publisher)) {
+	Write-Host '  OK' -ForegroundColor Green
+} Else {
+	vsce login $Publisher
+}
+
 
 Write-Host "`n3/3: Publish" -ForegroundColor Yellow
-Write-Host "`nToken: KeePass:" -ForegroundColor Cyan
+Write-Host "Token: KeePass:" -ForegroundColor Cyan
 Write-Host "MSMarketplacePublisher@jig.ch`n" -ForegroundColor Cyan
 vsce publish
 
